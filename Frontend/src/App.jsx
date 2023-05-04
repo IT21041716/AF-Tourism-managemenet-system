68623fe4f073f0ee38ba0ad9457348fd847ffb2a
sithanga rashmika <87372439+IT21041716@users.noreply.github.com>
2023-05-04 17:12:32 +0530

    push kala wade

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
@@ -10,6 +9,7 @@ import SignIn from './IT21041716/scenes/signin'
 import SignUp from './IT21041716/scenes/signup'
 import Sample from './IT21041716/scenes/sample'
 
+
 // hiruna
 import AddBlog from "./IT21049590/AddBlog";
 import UpdateBlog from "./IT21049590/UpdateBlog";
@@ -20,28 +20,30 @@ import AllBlogs from "./IT21049590/UserViewBlog";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Toaster position="top-center" reverseOrder={true} />
-      <BrowserRouter>
-        <Routes>
+          <BrowserRouter>
+            <Routes>
 
-          {/* hiruna */}
+              {/* hiruna */}
 
-          <Route path="/addBlog" element={<AddBlog />} />
-          <Route path="/UpdateBlog/:id" element={<UpdateBlog />} />
-          <Route path="/ViewBlogs" element={<ViewBlogs />} />
-          <Route path="/AllBlogs" element={<AllBlogs />} />
+              <Route path="/addBlog" element={<AddBlog />} />
+              <Route path="/UpdateBlog/:id" element={<UpdateBlog />} />
+              <Route path="/ViewBlogs" element={<ViewBlogs />} />
+              <Route path="/AllBlogs" element={<AllBlogs />} />
 
 
-          {/* sithanga */}
+              {/* sithanga */}
 
-          <Route path='/login' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/sample' element={<Sample />} />
+              <Route path='/login' element={<SignIn />} />
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/sample' element={<Sample />} />
 
-        </Routes>
-      </BrowserRouter>
-    </>
+
+
+            </Routes>
+          </BrowserRouter>
+    </div>
 
 
   );
