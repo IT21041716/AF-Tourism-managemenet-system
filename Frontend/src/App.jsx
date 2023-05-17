4b5f9c90efcb4471b834b61cce5f8c50365d8536
Hiruna Lalanga <IT21049590@my.sliit.lk>
2023-05-17 12:32:45 +0530

    Navbar complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,27 +4,21 @@ import { Toaster } from "react-hot-toast";
 
 import { useState } from "react";
 // import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-
 
 //sithanga
 import SignIn from "./IT21041716/scenes/signin";
 import SignUp from "./IT21041716/scenes/signup";
 //import Sample from "./IT21041716/scenes/sample";
 import Sample from "./IT21041716/scenes/sample";
-import Layout from './IT21041716/scenes/Cpanel/index'
-import Dashboard from './IT21041716/scenes/Cpanel/dashboard'
-import AddTrip from './IT21041716/scenes/Cpanel/Addtrip'
-import TripPlans from './IT21041716/scenes/Cpanel/TripPlans'
-import Revords from './IT21041716/scenes/Cpanel/RevOrders'
-import Acpords from './IT21041716/scenes/Cpanel/AcpOrders'
-import History from './IT21041716/scenes/Cpanel/OrderHistory'
-import Checkout from './IT21041716/scenes/checkout/index'
-import Test from './IT21041716/scenes/Cpanel/test'
+import Layout from "./IT21041716/scenes/Cpanel/index";
+import Dashboard from "./IT21041716/scenes/Cpanel/dashboard";
+import AddTrip from "./IT21041716/scenes/Cpanel/Addtrip";
+import TripPlans from "./IT21041716/scenes/Cpanel/TripPlans";
+import Revords from "./IT21041716/scenes/Cpanel/RevOrders";
+import Acpords from "./IT21041716/scenes/Cpanel/AcpOrders";
+import History from "./IT21041716/scenes/Cpanel/OrderHistory";
+import Checkout from "./IT21041716/scenes/checkout/index";
+import Test from "./IT21041716/scenes/Cpanel/test";
 
 // hiruna
 import AddBlog from "./IT21049590/AddBlog";
@@ -42,7 +36,7 @@ import Login from "./IT21042560/login";
 import AddPost from "./IT21042560/Add-Post";
 import UserProfile from "./IT21042560/User-Prifile";
 import Certificate from "./IT21042560/User-Certificate";
-import Test from "./IT21042560/sample";
+import Test1 from "./IT21042560/sample";
 //import Header from "./IT21042560/header";
 
 function App() {
@@ -64,7 +58,6 @@ function App() {
 
           {/* sithanga */}
 
-
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           {/* <Route path='/sample' element={<Sample />} /> */}
@@ -78,26 +71,27 @@ function App() {
           <Route path="/userLogin" element={<Login />} />
           <Route path="/user/post/add/:id" element={<AddPost />} />
           <Route path="/user/profile/:id" element={<UserProfile />} />
-          <Route path="/user/profile/certificate/:id" element={<Certificate />} />
-          <Route path="/user/profile/test/:id" element={<Test />} />
+          <Route
+            path="/user/profile/certificate/:id"
+            element={<Certificate />}
+          />
+          <Route path="/user/profile/test/:id" element={<Test1 />} />
 
-          <Route path='/login' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/sample' element={<Sample />} />
-          <Route path='/dash' element={<Layout />} />
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path='/test' element={<Test />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/sample" element={<Sample />} />
+          <Route path="/dash" element={<Layout />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/test" element={<Test />} />
           {/* seller controll panel */}
           <Route element={<Layout />}>
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/New Trips' element={<AddTrip />} />
-            <Route path='/Trip Plans' element={<TripPlans />} />
-            <Route path='/Received Orders' element={<Revords />} />
-            <Route path='/Accepted Orders' element={<Acpords />} />
-            <Route path='/Trip Histories' element={<History />} />
-
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/New Trips" element={<AddTrip />} />
+            <Route path="/Trip Plans" element={<TripPlans />} />
+            <Route path="/Received Orders" element={<Revords />} />
+            <Route path="/Accepted Orders" element={<Acpords />} />
+            <Route path="/Trip Histories" element={<History />} />
           </Route>
-
         </Routes>
       </BrowserRouter>
     </div>
--- Frontend/src/IT21041716/scenes/sample.jsx
+++ Frontend/src/IT21041716/scenes/sample.jsx
@@ -1,68 +1,68 @@
-// import React, { useEffect } from 'react'
-// import { Navbar, Nav, Container } from 'react-bootstrap'
-// import { NavLink, Link, useNavigate } from 'react-router-dom'
-// import { useDispatch, useSelector } from 'react-redux';
-// import { signout } from '../actions/authActions';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { isLoggedIn } from '../actions/authActions';
+import React, { useEffect } from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { NavLink, Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signout } from '../actions/authActions';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { isLoggedIn } from '../actions/authActions';
 
 
 
-// const Index = () => {
+const Index = () => {
 
-//     const navigate = useNavigate();
-//     const authenticated = useSelector(state => state.auth.authenticated);
-//     const dispatch = useDispatch();
-
-
-//     const logout = () => {
-//         dispatch(signout())
-//     }
+    const navigate = useNavigate();
+    const authenticated = useSelector(state => state.auth.authenticated);
+    const dispatch = useDispatch();
 
 
-
-//     const renderLoggedIn = () => {
-//         return (
-//             <Nav>
-//                 <NavLink className="nav-link" onClick={logout}>Sign out</NavLink>
-//             </Nav>
-//         );
-//     }
-//     const renderNonLoggedIn = () => {
-//         return (
-//             <Nav>
-//                 <NavLink to="/login" className="nav-link" >Sign In</NavLink>
-//                 <NavLink to="/signup" className="nav-link" >Sign Up</NavLink>
-
-//             </Nav>
-//         );
-//     }
-//     useEffect(() => {
-//         if (!authenticated) {
-//             dispatch(isLoggedIn());
-//         }
-//     }, []);
+    const logout = () => {
+        dispatch(signout())
+    }
 
 
-//     return (
-//         <>
 
-//             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-//                 <Container fluid>
-//                     <Navbar.Brand href="#home">TECHNO SOLUTION PVT LTD</Navbar.Brand>
-//                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//                     <Navbar.Collapse id="responsive-navbar-nav">
-//                         <Nav className="me-auto">
-//                             <Nav.Link onClick={() => navigate('/')}>HOME</Nav.Link>
+    const renderLoggedIn = () => {
+        return (
+            <Nav>
+                <NavLink className="nav-link" onClick={logout}>Sign out</NavLink>
+            </Nav>
+        );
+    }
+    const renderNonLoggedIn = () => {
+        return (
+            <Nav>
+                <NavLink to="/login" className="nav-link" >Sign In</NavLink>
+                <NavLink to="/signup" className="nav-link" >Sign Up</NavLink>
+
+            </Nav>
+        );
+    }
+    useEffect(() => {
+        if (!authenticated) {
+            dispatch(isLoggedIn());
+        }
+    }, []);
 
 
-//                         </Nav>
-//                         {authenticated ? renderLoggedIn() : renderNonLoggedIn()}
-//                     </Navbar.Collapse>
-//                 </Container>
-//             </Navbar>
-//         </>
-//     )
-// }
+    return (
+        <>
 
-// export default Index
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container fluid>
+                    <Navbar.Brand href="#home">TECHNO SOLUTION PVT LTD</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link onClick={() => navigate('/')}>HOME</Nav.Link>
+
+
+                        </Nav>
+                        {authenticated ? renderLoggedIn() : renderNonLoggedIn()}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Index
