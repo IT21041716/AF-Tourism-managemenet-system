35c6238ad7633a1ee9bca81992bc74e2e7b7b6b5
sithanga rashmika <87372439+IT21041716@users.noreply.github.com>
2023-05-18 12:49:04 +0530

    sithanga dashborad settings

--- Frontend/src/IT21041716/component/Navbar.jsx
+++ Frontend/src/IT21041716/component/Navbar.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
@@ -8,19 +6,16 @@ import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import FlexBetween from './FlexBetween';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppBar, IconButton, Toolbar, useTheme, Box, Button, Typography, Menu, MenuItem } from '@mui/material';
-import { switchMode } from '../actions/generalAction';
 import { signout } from '../actions/authActions';
 
 const Navbar = (props) => {
-    const mode = useSelector(state => state.general.mode)
-    const dispatch = useDispatch()
-    const theme = useTheme()
-
+    const dispatch = useDispatch();
     const [anchorEl, setAnchorEl] = useState(null);
     const isOpen = Boolean(anchorEl);
     const handleClick = (event) => setAnchorEl(event.currentTarget);
-    const handleClose = () => setAnchorEl(null)
-    const menuOnClick = () => dispatch(signout())
+    const handleClose = () => setAnchorEl(null);
+    const menuOnClick = () => dispatch(signout());
+
     return (
         <AppBar sx={{
             position: 'static',
@@ -30,24 +25,37 @@ const Navbar = (props) => {
             <Toolbar sx={{ justifyContent: 'space-between' }}>
                 <FlexBetween>
                     <IconButton onClick={() => props.setIsSidebarOpen(!props.isSidebarOpen)} >
-                        <MenuOutlinedIcon  sx={{ color:"white"}}/>
+                        <MenuOutlinedIcon sx={{ color: "white" }} />
                     </IconButton>
                 </FlexBetween>
 
-                <FlexBetween
-                    gap='1.5rem'
-                >
-        
+                <FlexBetween gap='1.5rem'>
                     <IconButton>
-                        <NotificationsOutlinedIcon sx={{ color:"white"}} />
+                        <NotificationsOutlinedIcon sx={{ color: "white" }} />
                     </IconButton>
 
                     <IconButton>
-                        <SettingsOutlinedIcon sx={{ color:"white"}} />
+                        <SettingsOutlinedIcon sx={{ color: "white" }} />
                     </IconButton>
-                    <IconButton>
-                        <PersonOutlineOutlinedIcon sx={{ color:"white"}} />
+
+                    <IconButton
+                        aria-controls="profile-menu"
+                        aria-haspopup="true"
+                        onClick={handleClick}
+                    >
+                        <PersonOutlineOutlinedIcon sx={{ color: "white" }} />
                     </IconButton>
+
+                    <Menu
+                        id="profile-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={isOpen}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>View Profile</MenuItem>
+                        <MenuItem onClick={menuOnClick}>Logout</MenuItem>
+                    </Menu>
                 </FlexBetween>
             </Toolbar>
         </AppBar>
