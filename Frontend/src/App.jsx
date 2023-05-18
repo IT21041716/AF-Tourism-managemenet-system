dd0d7ddac0ce6deb7a0eeabadc1be8ac1f6a4618
Hiruna Lalanga <IT21049590@my.sliit.lk>
2023-05-18 11:27:39 +0530

    d

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-
-
 //sithanga
 import SignIn from "./IT21041716/scenes/signin";
 import SignUp from "./IT21041716/scenes/signup";
@@ -46,7 +44,7 @@ function App() {
           <Route path="/addBlog" element={<AddBlog />} />
           <Route path="/UpdateBlog/:id" element={<UpdateBlog />} />
           <Route path="/ViewBlogs" element={<ViewBlogs />} />
-          <Route path="/AllBlogs" element={<AllBlogs />} />
+          <Route path="/AllBlogs/:id" element={<AllBlogs />} />
           <Route path="/Feedback/:id" element={<Feedback />} />
           <Route path="/BlogFeedbacks/:id" element={<BlogFeedbacks />} />
           <Route path="/FeedbackItem" element={<FeedbackItem />} />
@@ -67,27 +65,28 @@ function App() {
           <Route path="/userLogin" element={<Login />} />
           <Route path="/user/post/add/:id" element={<AddPost />} />
           <Route path="/user/profile/:id" element={<UserProfile />} />
-          <Route path="/user/profile/certificate/:id" element={<Certificate />} />
+          <Route
+            path="/user/profile/certificate/:id"
+            element={<Certificate />}
+          />
           <Route path="/user/profile/test/:id" element={<Test />} />
 
-
-          <Route path='/login' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/sample' element={<Sample />} />
-          <Route path='/dash' element={<Layout />} />
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path='/test' element={<Test />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/sample" element={<Sample />} />
+          <Route path="/dash" element={<Layout />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/test" element={<Test />} />
           {/* seller controll panel */}
           <Route element={<Layout />}>
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/New Trips' element={<AddTrip />} />
-            <Route path='/Trip Plans' element={<TripPlans />} />
-            <Route path='/Received Orders' element={<Revords />} />
-            <Route path='/Accepted Orders' element={<Acpords />} />
-            <Route path='/Trip Histories' element={<History />} />
-
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/New Trips" element={<AddTrip />} />
+            <Route path="/Trip Plans" element={<TripPlans />} />
+            <Route path="/Received Orders" element={<Revords />} />
+            <Route path="/Accepted Orders" element={<Acpords />} />
+            <Route path="/Trip Histories" element={<History />} />
           </Route>
-
         </Routes>
       </BrowserRouter>
     </div>
